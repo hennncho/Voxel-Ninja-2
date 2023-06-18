c210dde9590dac97f0772030057a7e8731d0bd9c
hennncho <khetagkambolov@gmail.com>
2023-06-18 22:55:28 +0300

    2.0
    Все нахуй завтра защита диплома ебаный в рот

--- Voxel Ninja 2/Assets/Scripts/Scenes/GameOver.cs
+++ Voxel Ninja 2/Assets/Scripts/Scenes/GameOver.cs
@@ -8,11 +8,13 @@ public class GameOver : MonoBehaviour
     [SerializeField] private GameObject gameOverPanel;
     [SerializeField] private TMP_Text distanceText;
     [SerializeField] private TMP_Text killCountText;
+    [SerializeField] private Pause pause;
     [HideInInspector] public float distance;
     [HideInInspector] public int killCount;    
 
     public void Defeat()
-    {        
+    {
+        pause.PauseOn();
         distanceText.text = "Distance\n" + distance.ToString();
         killCountText.text = "Enemies killed\n" + killCount.ToString();
         gameOverPanel.SetActive(true);        
